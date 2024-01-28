cd177bb5479e363f44d565cc73a03ad9a32ef836
Noa Kim <cczu@umich.edu>
2024-01-27 20:43:14 -0500

    Update portfolio.js
    added first round of info

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://noa-kim.github.io',
+  title: 'Noa Kim, Product Design Manager',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Noa Kim, MSI',
+  role: 'Product Manager, Healthcare Design',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+  resume: '#',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/noakim/',
+    // github: 'https://github.com',
   },
 }
 
@@ -21,20 +21,20 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Diabetes Cost and Coverage Navigator',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+      'A suite of tools developed for primary care teams to navigate the burdensome process of diabetes medication and supplies coverage, cost, prior authorization, and DME ordering',
     stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    // sourceCode: 'https://github.com',
+    livePreview: 'https://www.mct2d.org/resource-library?media-type=&tags=Coverage_Guides&target-audience=&collections=&page=1',
   },
   {
-    name: 'Project 2',
+    name: 'Low Carb Jumpstart',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+      'A digital behavioral health tool to jumpstart a low carbohydrate lifestyle for individuals with type 2 diabetes',
     stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    // sourceCode: 'https://github.com',
+    livePreview: 'https://jumpstart.mct2d.org/',
   },
   {
     name: 'Project 3',
@@ -65,7 +65,7 @@ const skills = [
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'hifromnoa@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
